ccaa971023c4bbf82b016a500282c7c5c169df42
superflowercodex <superflowercodex@gmail.com>
2026-07-29 21:44:48 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,8 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js';
+<script type="module">
+  import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-export const supabase = createClient(
-  'https://zdlaloiruxamxsduxqxw.supabase.co',
-  'sb_publishable_EA8w0ho2lQ19ivGLyOCZoA_jqeKT79d'
-);
+  export const supabase = createClient(
+    "https://zdlaloiruxamxsduxqxw.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpkbGFsb2lydXhhbXhzZHV4cXh3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUzMTc3MTEsImV4cCI6MjEwMDg5MzcxMX0.m1MdepLYwqZuu3fGVzDfaFUkdW4rSsKt1Z85UDbpRBQ"
+  );
+</script>

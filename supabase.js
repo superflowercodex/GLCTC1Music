55e9c2dcfcee81e16628c9c518af0b9b6cd88c06
superflowercodex <superflowercodex@gmail.com>
2026-07-29 12:45:17 +0100

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from 'https://esm.sh/@supabase/supabase-js';
+
+export const supabase = createClient(
+  'https://zdlaloiruxamxsduxqxw.supabase.co',
+  'sb_publishable_EA8w0ho2lQ19ivGLyOCZoA_jqeKT79d'
+);
